d550dfacc6885d3c6488d06261db8fe525d6dc87
Dedealtograu <matias.jms14@gmail.com>
2025-08-02 10:27:24 -0300

    Final da aula 234

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,4 +1,9 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator'
+
 export class LoginDto {
+  @IsEmail({}, { message: 'E-mail inválido' })
   email: string
+  @IsString({ message: 'Senha deve ser uma string' })
+  @IsNotEmpty({ message: 'Senha não pode ser vazia' })
   password: string
 }
