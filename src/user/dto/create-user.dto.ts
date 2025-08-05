af422d418fc3e6909148c8994fb8282eb18b6c8a
Dedealtograu <matias.jms14@gmail.com>
2025-08-04 22:32:44 -0300

    Final da aula 238

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,12 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator'
+
+export class CreateUserDto {
+  @IsString({ message: 'Nome deve ser uma string' })
+  @IsNotEmpty({ message: 'Nome não pode ser vazio' })
+  name: string
+  @IsEmail({}, { message: 'Email inválido' })
+  email: string
+  @IsString({ message: 'Senha deve ser uma string' })
+  @IsNotEmpty({ message: 'Senha não pode ser vazia' })
+  password: string
+}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common'
 import { UserService } from './user.service'
 import { UserController } from './user.controller'
-import { ConfigModule } from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { User } from './entities/user.entity'
+import { CommonModule } from 'src/common/common.module'
 
 @Module({
-  imports: [ConfigModule],
+  imports: [TypeOrmModule.forFeature([User]), CommonModule],
   providers: [UserService],
   controllers: [UserController],
 })
