e2009820b203806967d0c87c86e84f3d947f549e
Dedealtograu <matias.jms14@gmail.com>
2025-08-08 10:52:05 -0300

    Final da aula 248

--- src/post/dto/create-post.dto.ts
+++ src/post/dto/create-post.dto.ts
@@ -1,1 +1,19 @@
-export class CreatePostDto {}
+import { IsNotEmpty, IsOptional, IsString, IsUrl, Length } from 'class-validator'
+
+export class CreatePostDto {
+  @IsString({ message: 'O campo title deve ser uma string' })
+  @Length(10, 150, { message: 'O campo title deve ter entre 10 e 150 caracteres' })
+  title: string
+
+  @IsString({ message: 'O campo excerpt deve ser uma string' })
+  @Length(10, 200, { message: 'O campo excerpt deve ter entre 10 e 200 caracteres' })
+  excerpt: string
+
+  @IsString({ message: 'O campo content deve ser uma string' })
+  @IsNotEmpty({ message: 'O campo content deve ser preenchido' })
+  content: string
+
+  @IsOptional()
+  @IsUrl({ require_tld: false })
+  coverImage: string
+}
