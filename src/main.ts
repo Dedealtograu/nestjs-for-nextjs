031ba6fd3ed688ed834f03ca0e108b06e8345b42
Dedealtograu <matias.jms14@gmail.com>
2025-08-12 10:23:53 -0300

    Final da aula 258

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 import { ValidationPipe } from '@nestjs/common'
 import helmet from 'helmet'
+import { parseCorsWhitelist } from './common/utils/parse-cors-whitelist'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
@@ -12,12 +13,20 @@ async function bootstrap() {
     }),
   )
 
+  const corsWhitelist = parseCorsWhitelist(process.env.CORS_WHITELIST || '')
+
   app.enableCors({
-    origin: '*',
+    origin: (origin: string | undefined, callback: (...args: any[]) => void) => {
+      if (!origin || corsWhitelist.includes(origin)) {
+        callback(null, true)
+      }
+
+      return callback(new Error('Not allowed by CORS'), false)
+    },
   })
 
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, forbidNonWhitelisted: true }))
 
-  await app.listen(process.env.PORT ?? 3000)
+  await app.listen(process.env.APP_PORT ?? 3001)
 }
 void bootstrap()
